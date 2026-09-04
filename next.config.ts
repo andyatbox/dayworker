e099e9235bd4ab1b13bc20aaeeb734f5a920b9d2
BOXNYC <joe@box.biz>
2026-09-04 01:27:07 -0400

    Made app required updates for @mike to this this by  @andy.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,24 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      { "source": "/user/:path*", "destination": "/job-landing.html" },
+      { "source": "/job/:path*", "destination": "/job-landing.html" },
+      { "source": "/privacy", "destination": "/privacy.html" },
+      { "source": "/privacy/", "destination": "/privacy.html" },
+      { "source": "/terms", "destination": "/terms.html" },
+      { "source": "/terms/", "destination": "/terms.html" }
+    ];
+  },
+  async redirects() {
+    return [{
+      "source": "/((?!\\.well-known|job|user).*)",
+      "has": [{ "type": "host", "value": "dayworker.co" }],
+      "destination": "https://www.dayworker.co/$1",
+      "permanent": true
+    }]
+  }
+};
 
 export default nextConfig;
